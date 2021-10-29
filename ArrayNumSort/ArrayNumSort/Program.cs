e5dcd6c3e84a39f658a2b273ec8b091e24c23f8a
Jun <foriv808@gmail.com>
2021-10-29 12:39:31 +0800

    Array Number OrderBy Final

---
+++ ArrayNumSort/ArrayNumSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayNumSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 5, 6, 5, 6, 3, 9, 8 };
+
+            Array.Sort(nums);
+            foreach (var val in nums)
+            {
+                Console.Write(val + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
